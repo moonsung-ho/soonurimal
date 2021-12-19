dc4e32ff5d47c4a2da8dc436f3043081b546b6ed
Sungho Moon <moon7959@kakao.com>
2021-12-19 16:34:48 +0900

    init

---
+++ script.js
@@ -0,0 +1,47 @@
+const ul = document.querySelector('ul');
+const input = document.querySelector("input#input");
+const recommended_soonurimal = document.querySelector("p#recommended_soonurimal")
+
+if (localStorage.getItem("recent_word")) {
+    recommended_soonurimal.innerHTML = "최근 검색한 순우리말: " + localStorage.getItem("recent_word")
+}
+
+document.querySelector("button#submit").addEventListener("click", () => {
+    search()
+})
+document.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        search()
+    }
+})
+
+function search() {
+    recommended_soonurimal.remove()
+    document.querySelector("b#searched_word").innerHTML = input.value
+    ul.innerHTML = "";
+    let li = document.createElement("li");
+    li.innerHTML = '검색중...'
+    ul.appendChild(li)
+    let word = input.value;
+    let url = `http://opendict.korean.go.kr/api/search?certkey_no=3400&key=D34F025B51024A8569060A7DB7BF851A&target_type=search&req_type=json&part=word&q=${word}&sort=dict&start=1&num=10&advanced=y&type2=native`
+    fetch("https://soonurimal-cors.herokuapp.com/" + url)
+        .then(response => response.json())
+        .then(data => {
+            localStorage.setItem("recent_word", data.channel.item[0].word)
+            ul.innerHTML = ""
+            let n = 0
+            while (n < data.channel.item.length) {
+                let li = document.createElement("li");
+                li.innerHTML = `${n + 1}. ${data.channel.item[n].sense[0].definition}`
+                ul.appendChild(li)
+                n++
+            }
+        })
+        .catch(err => {
+            ul.innerHTML = ""
+            let li = document.createElement("li");
+            li.innerHTML = '검색중 오류가 발생했거나 검색어가 순우리말이 아닙니다. 검색어를 확인해주세요.'
+            ul.appendChild(li)
+            console.error(err)
+        })
+}
